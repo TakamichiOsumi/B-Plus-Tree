8f9d2102ff3d53b3d6375e519e88de528df75139
Takamichi Osumi <takamichiosumi01@gmail.com>
2024-07-01 12:43:41 +0900

    Added the reverse order tests for larger data volume.

--- tests/keys_bpt_app.c
+++ tests/keys_bpt_app.c
@@ -860,12 +860,10 @@ keys_test_more_data(void){
 		    employee_free, 3);
 
     for (i = 1; i < max; i++){
-	printf("debug : app inserts key = %lu\n", i);
 	assert(bpt_insert(tree, (void *) i, (void *) &emp) == true);
     }
 
     for (i = max - 1; i >= 1; i--){
-	printf("debug : app searches key = %lu\n", i);
 	assert(bpt_search(tree, (void *) 1, &node) == true);
     }
 
@@ -880,6 +878,23 @@ keys_test_more_data(void){
     }
 
     assert(ll_get_length(tree->root->keys) == 0);
+
+    /*
+     * Do the same thing in reverse order.
+     */
+    for (i = max; i >= 1; i--){
+	assert(bpt_insert(tree, (void *) i, (void *) &emp) == true);
+    }
+
+    for (i = max; i >= 1; i--){
+	printf("debug : app deletes key = %lu\n", i);
+	assert(bpt_delete(tree, (void *) i) == true);
+	/* Detect any incorrect tree structure */
+	for (j = i - 1; j >= 1; j--){
+	    assert(bpt_search(tree, (void *) j, &node) == true);
+	}
+	printf("debug : proved the tree has the valid searchability\n");
+    }
 }
 
 static void
